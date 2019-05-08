8f500d75386c7618e30d2b06bdb9a674e45fd6d3
yuanzhaohao <yuanzhaohao@163.com>
2019-05-08 16:20:33 +0800

    ! feat: use dashkit beta

---
+++ src/components/list/list-route.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Card } from 'dashkit-ui';
+import { connect } from 'react-redux';
+import { routerActions } from 'react-router-redux';
+import * as indexActions from '../../redux/list/effects';
+import { RootState } from '../../redux/reducers';
+import { WithRedux } from '../../redux/typings';
+const mapStateToProps = ({ list }: RootState) => {
+  return {
+    list,
+  };
+}
+const mapDispatchToProps = {
+  ...indexActions,
+  ...routerActions,
+};
+type Props = WithRedux<typeof mapStateToProps, typeof mapDispatchToProps>;
+
+class ListRoute extends React.Component<Props> {
+  render() {
+    const { shortestRoutes } = this.props.list;
+    return (
+      shortestRoutes && shortestRoutes.length
+      ? <Card.Collapse className="route-panel">
+        {shortestRoutes.map((route, index) =>
+          <Card key={route.join(',')} index={index}>
+            <Card.Header>Route {index + 1}</Card.Header>
+            <Card.Body>{route.join(',')}</Card.Body>
+          </Card>
+        )}
+      </Card.Collapse>
+      : null
+    );
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ListRoute);
---
+++ src/components/list/list-select.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { Select } from 'dashkit-ui';
+import { connect } from 'react-redux';
+import { routerActions } from 'react-router-redux';
+import * as indexActions from '../../redux/list/effects';
+import { RootState } from '../../redux/reducers';
+import { WithRedux } from '../../redux/typings';
+
+const { Option, OptionGroup } = Select;
+const mapStateToProps = ({ list }: RootState) => {
+  return {
+    list,
+  };
+}
+const mapDispatchToProps = {
+  ...indexActions,
+  ...routerActions,
+};
+type Props = WithRedux<typeof mapStateToProps, typeof mapDispatchToProps>;
+
+class ListSelect extends React.Component<Props> {
+  render() {
+    const { lineData, fromStation, toStation } = this.props.list;
+    return (
+      <div className="search-station-select">
+        <Select
+          placeholder="Starting Point"
+          value={fromStation}
+          onChange={this.handleStationChange.bind(this, 'fromStation')}
+          className="from-station"
+          prefix="circle"
+          prefixClassName="prefix-from"
+        >
+          {Object.keys(lineData).map(lineKey =>
+            <OptionGroup label={lineKey} key={lineKey}>
+              {lineData[lineKey].stations.map(station =>
+                <Option key={station} value={station} filterOption={this.filterOption}>{station}</Option>
+              )}
+            </OptionGroup>
+          )}
+        </Select>
+        <Select
+          placeholder="Destination"
+          value={toStation}
+          onChange={this.handleStationChange.bind(this, 'toStation')}
+          className="to-station"
+          prefix="map-pin"
+          prefixClassName="prefix-to"
+        >
+          {Object.keys(lineData).map(lineKey =>
+            <OptionGroup label={lineKey} key={lineKey}>
+              {lineData[lineKey].stations.map(station =>
+                <Option key={station} value={station} filterOption={this.filterOption}>{station}</Option>
+              )}
+            </OptionGroup>
+          )}
+        </Select>
+      </div>
+    );
+  }
+
+
+
+  filterOption(inputValue: string, value: string) {
+    return value.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1;
+  }
+
+  handleStationChange(key: string, value: any) {
+    this.props.updateStation(value, key);
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ListSelect);
